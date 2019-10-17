c00dc8547bca16a2a8b993b14d5c400147d238ca
Anil Pacaci <apacaci@uwaterloo.ca>
2019-10-16 22:53:46 -0400

    Sliding window reservoir sampling

---
+++ src/test/java/ca/uwaterloo/cs/streamingrpq/WindowedSpanningTreeTestRSPQ.java
@@ -0,0 +1,58 @@
+package ca.uwaterloo.cs.streamingrpq;
+
+import ca.uwaterloo.cs.streamingrpq.input.InputTuple;
+import ca.uwaterloo.cs.streamingrpq.input.SimpleTextStream;
+import ca.uwaterloo.cs.streamingrpq.input.TextStream;
+import ca.uwaterloo.cs.streamingrpq.stree.data.QueryAutomata;
+import ca.uwaterloo.cs.streamingrpq.stree.engine.RPQEngine;
+import ca.uwaterloo.cs.streamingrpq.stree.engine.WindowedRSPQ;
+import com.codahale.metrics.ConsoleReporter;
+import com.codahale.metrics.MetricRegistry;
+
+import java.util.concurrent.TimeUnit;
+
+public class WindowedSpanningTreeTestRSPQ {
+
+    static String filename = "src/main/resources/examplegraph.txt";
+
+    public static void main(String[] args) {
+        QueryAutomata<String> query = new QueryAutomata<String>(3);
+        query.addFinalState(0);
+        query.addFinalState(1);
+        query.addFinalState(2);
+        query.addTransition(0, "m", 0);
+        query.addTransition(0, "f", 1);
+        query.addTransition(1, "f", 1);
+        query.addTransition(1, "m", 2);
+        query.addTransition(2, "m", 2);
+
+        RPQEngine<String> rapqEngine = new WindowedRSPQ<>(query, 100, 10, 1, 10);
+        MetricRegistry metricRegistry = new MetricRegistry();
+        rapqEngine.addMetricRegistry(metricRegistry);
+
+        final ConsoleReporter reporter = ConsoleReporter.forRegistry(metricRegistry)
+                .convertRatesTo(TimeUnit.SECONDS)
+                .convertDurationsTo(TimeUnit.MILLISECONDS)
+                .build();
+        reporter.start(1, TimeUnit.MINUTES);
+
+
+        TextStream stream = new SimpleTextStream();
+        stream.open(filename);
+        InputTuple<Integer, Integer, String> input = stream.next();
+
+        while (input != null) {
+            rapqEngine.processEdge(input);
+            input = stream.next();
+        }
+
+        rapqEngine.getResults().iterator().forEachRemaining(t-> {System.out.println(t.getSource() + " --> " + t.getTarget());});
+
+        rapqEngine.shutDown();
+
+        stream.close();
+
+        reporter.stop();
+
+    }
+}
