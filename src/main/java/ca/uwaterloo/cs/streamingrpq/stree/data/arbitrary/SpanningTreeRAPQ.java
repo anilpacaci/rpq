114870efcb0c179a049d1da04aeb62faa5473aab
Anil Pacaci <apacaci@uwaterloo.ca>
2020-01-13 17:09:25 -0500

    Spanning tree abstract implementation, now move to Delta

--- src/main/java/ca/uwaterloo/cs/streamingrpq/stree/data/arbitrary/SpanningTreeRAPQ.java
+++ src/main/java/ca/uwaterloo/cs/streamingrpq/stree/data/arbitrary/SpanningTreeRAPQ.java
@@ -1,9 +1,6 @@
 package ca.uwaterloo.cs.streamingrpq.stree.data.arbitrary;
 
-import ca.uwaterloo.cs.streamingrpq.stree.data.AbstractSpanningTree;
-import ca.uwaterloo.cs.streamingrpq.stree.data.GraphEdge;
-import ca.uwaterloo.cs.streamingrpq.stree.data.ProductGraph;
-import ca.uwaterloo.cs.streamingrpq.stree.data.ProductGraphNode;
+import ca.uwaterloo.cs.streamingrpq.stree.data.*;
 import ca.uwaterloo.cs.streamingrpq.stree.util.Constants;
 import ca.uwaterloo.cs.streamingrpq.stree.util.Hasher;
 import org.slf4j.Logger;
@@ -13,48 +10,38 @@ import java.util.*;
 
 public class SpanningTreeRAPQ<V> extends AbstractSpanningTree<V> {
 
-    protected SpanningTreeRAPQ() {
-        // empty constructor
-    }
-
     protected SpanningTreeRAPQ(DeltaRAPQ<V> deltaRAPQ, V rootVertex, long timestamp) {
-        this.rootNode = new TreeNode<V>(rootVertex, 0, null, this, timestamp);
+        super(timestamp);
+
+        TreeNode<V> root = new TreeNode<V>(rootVertex, 0, null, this, timestamp);
+        this.rootNode = root;
         this.deltaRAPQ = deltaRAPQ;
-        this.nodeIndex = new HashMap<>(Constants.EXPECTED_TREE_SIZE);
-        nodeIndex.put(Hasher.createTreeNodePairKey(rootVertex, 0), rootNode);
-        this.minTimestamp = timestamp;
+        nodeIndex.put(Hasher.createTreeNodePairKey(rootVertex, 0), root);
 
         candidates = new HashSet<>(Constants.EXPECTED_TREE_SIZE);
         candidateRemoval = new HashSet<>(Constants.EXPECTED_TREE_SIZE);
         visited = new HashSet<>(Constants.EXPECTED_TREE_SIZE);
     }
 
-    /**
-     * removes old edges from the productGraph, used during window management.
-     * This function assumes that expired edges are removed from the productGraph, so traversal assumes that it is guarenteed to
-     * traverse valid edges
-     * @param minTimestamp lower bound of the window interval. Any edge whose timestamp is smaller will be removed
-     * @return The set of nodes that have expired from the window as there is no other path
-     */
-    public <L> Collection<TreeNode<V>> removeOldEdges(long minTimestamp, ProductGraph<V,L> productGraph) {
-        // if root is expired (root node timestamp is its youngest edge), then the entire tree needs to be removed
-//        if(this.rootNode.getTimestamp() <= minTimestamp) {
-//            return this.nodeIndex.values();
-//        }
+    @Override
+    protected AbstractTreeNode<V> createNewTreeNode(V vertex, int state, AbstractTreeNode<V> parentNode, long timestamp) {
+        TreeNode<V> child = new TreeNode<V>(vertex, state, (TreeNode) parentNode, this, timestamp);
+        return child;
+    }
 
-        // potentially expired nodes
-        candidates.clear();
-        candidateRemoval.clear();
-
+    @Override
+    protected long populateCandidateRemovals(long minTimestamp) {
         // perform a bfs traversal on tree, no need for visited as it is a three
-        LinkedList<TreeNode> queue = new LinkedList<>();
+        LinkedList<AbstractTreeNode<V>> queue = new LinkedList<>();
+
         // minTimestamp of the tree should be updated, find the lowest timestamp in the tree higher than the minTimestmap
         // because after this maintenance, there is not going to be a node in the tree lower than the minTimestamp
         long minimumValidTimetamp = Long.MAX_VALUE;
         queue.addAll(rootNode.getChildren());
+
         while(!queue.isEmpty()) {
             // populate the queue with children
-            TreeNode currentVertex = queue.remove();
+            AbstractTreeNode<V> currentVertex = queue.remove();
             queue.addAll(currentVertex.getChildren());
 
             // check time timestamp to decide whether it is expired
@@ -66,113 +53,8 @@ public class SpanningTreeRAPQ<V> extends AbstractSpanningTree<V> {
                 minimumValidTimetamp = currentVertex.getTimestamp();
             }
         }
-        //update the lowest minimum timestamp for this tree
-        this.minTimestamp = minimumValidTimetamp;
 
-        Iterator<TreeNode<V>> candidateIterator = candidates.iterator();
-        this.visited.clear();
-
-        LOG.debug("Expiry for spanning tree {}, # of candidates {} out of {} nodes", toString(), candidates.size(), nodeIndex.size());
-
-        //scan over potential nodes once.
-        // For each potential, check they have a valid non-tree edge in the original productGraph
-        // If there is traverse down from here (in the productGraph) and remove all children from potentials
-        while(candidateIterator.hasNext()) {
-            TreeNode<V> candidate = candidateIterator.next();
-            // check if a previous traversal already found a path for the candidate
-            if(candidate.getTimestamp() > minTimestamp) {
-                continue;
-            }
-            //check if there exists any incoming edge from a valid state
-            Collection<GraphEdge<ProductGraphNode<V>>> backwardEdges = productGraph.getBackwardEdges(candidate.getVertex(), candidate.getState());
-            TreeNode<V> newParent = null;
-            GraphEdge<ProductGraphNode<V>> newParentEdge = null;
-            for(GraphEdge<ProductGraphNode<V>> backwardEdge : backwardEdges) {
-                newParent = this.getNode(backwardEdge.getSource().getVertex(), backwardEdge.getSource().getState());
-                if (newParent != null && (!candidates.contains(newParent) || candidateRemoval.contains(newParent))) {
-                    // there is an incoming edge with valid source
-                    // source is valid (in the tree) and not in candidate
-                    newParentEdge = backwardEdge;
-                    break;
-                }
-            }
-
-            //if this node becomes valid, just traverse everything down in the productGraph. If somehow I traverse an edge who would
-            // be an incoming edge of some candidate, then it is removed from candidate so I never check it there.
-            // If that edge is checked during incoming edge search, than it might be only examined again with a traversal which makes sure
-            // that edge cannot be visited again. Therefore it is O(m)
-            if(newParentEdge != null) {
-                // means that there was a tree node that is not in the candidates but in the tree as a valid node
-                candidate.setParent(newParent);
-                candidate.setTimestamp(Long.min(newParent.getTimestamp(), newParentEdge.getTimestamp()));
-                // current vertex has a valid incoming edge, so it needs to be removed from candidates
-                candidateRemoval.add(candidate);
-
-                //now traverse the productGraph down from this node, and remove any visited node from candidates
-                LinkedList<TreeNode> traversalQueue = new LinkedList<TreeNode>();
-
-                // initial node is the current candidate, because now it is reachable
-                traversalQueue.add(candidate);
-                while(!traversalQueue.isEmpty()){
-                    TreeNode<V> currentVertex = traversalQueue.remove();
-                    visited.add(currentVertex);
-
-                    Collection<GraphEdge<ProductGraphNode<V>>> forwardEdges = productGraph.getForwardEdges(currentVertex.getVertex(), currentVertex.getState());
-                    // for each potential child
-                    for(GraphEdge<ProductGraphNode<V>> forwardEdge : forwardEdges) {
-                        // I can simply retrieve from the tree index because any node that is reachable are in tree index
-                        TreeNode<V> outgoingTreeNode = this.getNode(forwardEdge.getTarget().getVertex(), forwardEdge.getTarget().getState());
-                        // there exists such node in the tree & the edge we are traversing is valid & this node has not been visited before
-                        if (outgoingTreeNode != null && forwardEdge.getTimestamp() > minTimestamp && !visited.contains(outgoingTreeNode)) {
-                            if (candidates.contains(outgoingTreeNode)) {
-                                // remove this node from potentials as now there is a younger path
-                                candidateRemoval.add(outgoingTreeNode);
-                            }
-                            if (outgoingTreeNode.getTimestamp() < Long.min(currentVertex.getTimestamp(), forwardEdge.getTimestamp())) {
-                                // note anything in the candidates has a lower timestamp then
-                                // min(currentVertex, forwardEdge) as currentVertex and forward edge are guarenteed to be larger than minTimestamp
-                                outgoingTreeNode.setParent(currentVertex);
-                                outgoingTreeNode.setTimestamp(Long.min(currentVertex.getTimestamp(), forwardEdge.getTimestamp()));
-                                traversalQueue.add(outgoingTreeNode);
-                            }
-                        }
-                        // nodes with forward edge smaller than minTimestamp with already younger paths no need to be visited
-                        // so no need to add them into the queue
-                    }
-                }
-
-            }
-
-        }
-
-        // update candidates
-        candidates.removeAll(candidateRemoval);
-
-        // now if there is any potential remanining, it is guarenteed that they are not reachable
-        // so simply clean the indexes and generate negative result if necessary
-        for(TreeNode<V> currentVertex : candidates) {
-            // remove this node from the node index
-            nodeIndex.remove(Hasher.getThreadLocalTreeNodePairKey(currentVertex.getVertex(), currentVertex.getState()));
-            //remove this node from parent's chilren list
-            currentVertex.setParent(null);
-
-            //remove this entry from nodeToTreeIndex
-            this.deltaRAPQ.removeFromTreeIndex(currentVertex, this);
-        }
-
-        if(this.isExpired(minTimestamp)) {
-            TreeNode<V> removedTuple = this.getRootNode();
-            deltaRAPQ.removeTree(this);
-        }
-
-        LOG.debug("Spanning tree rooted at {}, remove {} nodes at timestamp {} ", getRootVertex(), candidates.size(), minTimestamp);
-
-        return candidates;
-    }
-
-    @Override
-    public String toString() {
-        return new StringBuilder().append("Root:").append(rootNode.getVertex()).append("-TS:").append(rootNode.getTimestamp()).toString();
+        return minimumValidTimetamp;
     }
 
 }
