4644401719f9fa238a9d8e23c1adeaef8acbedb4
Anil Pacaci <apacaci@uwaterloo.ca>
2020-01-23 21:17:44 -0500

    Cleanup old imports - package built on Java13

--- src/main/java/ca/uwaterloo/cs/streamingrpq/input/TextFileStream.java
+++ src/main/java/ca/uwaterloo/cs/streamingrpq/input/TextFileStream.java
@@ -5,7 +5,6 @@ import com.google.common.base.Splitter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.xml.soap.Text;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
